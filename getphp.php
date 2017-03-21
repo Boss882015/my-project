d89ef612cc8473d6dc60fd4f65284447a9c1a1cf
duc <tukhigapem8720@gmail.com>
2017-03-21 19:50:38 +0700

    add project

---
+++ getphp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/21/2017
+ * Time: 7:45 PM
+ */
+echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"];
+?>
---
+++ postphp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/21/2017
+ * Time: 7:38 PM
+ */
+$data=$_POST;
+echo "<pre>";
+var_dump($data);
+?>
