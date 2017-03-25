9dcb294f0cd522460b567b17057475f1d2761037
admin <tukhigapem8720@gmail.com>
2017-03-25 20:03:01 +0700

    25/3/2017

---
+++ selectsql.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 3/25/2017
+ * Time: 7:38 PM
+ */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "my_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
